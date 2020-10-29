92106ed7cfbbd262cc666dc710367b9304c47201
Vaibhav Saxena <Vaibhav.1865@gmail.com>
2020-10-29 08:44:56 +0530

    modified 27th October python Code with Dynamic input

--- Python/27thOct.py
+++ Python/27thOct.py
@@ -1,7 +1,8 @@
 #Function to generate and print all the binary numbers from 1 to n, given n, using the inbuilt bin() function.
-def toBin(n):
+def to_Binary(n):
 	for i in range(1,n+1):
 		print(bin(i)[2:])
 		
 # Driver program to test the above function 
-toBin(5)
+n=input("Enter Value of n")
+to_Binary(n)
